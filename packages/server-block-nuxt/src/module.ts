ad68583c845575c89f1c7c5ebe4685488e90e01b
Hebilicious <xsh4k3@gmail.com>
2023-07-15 17:18:18 +0700

    fix: properly add nitro handlers (#8)

--- packages/server-block-nuxt/src/module.ts
+++ packages/server-block-nuxt/src/module.ts
@@ -1,11 +1,24 @@
 import { existsSync, promises as fsp } from "node:fs"
-
-import { addPlugin, addServerHandler, createResolver, defineNuxtModule, useNitro } from "@nuxt/kit"
+import { resolve as pathResolve } from "node:path"
+import { addPlugin, createResolver, defineNuxtModule, useNitro } from "@nuxt/kit"
 import ExtractSFCBlock from "@hebilicious/extract-sfc-block"
 
 import { loadFile } from "magicast"
 import type { NitroEventHandler } from "nitropack"
 import { SupportedMethods, getRoute, logger, makePathShortener, writeHandlers } from "./utils"
+
+export async function* walkFiles(dir: string): AsyncGenerator<string> {
+  const entries = await fsp.readdir(dir, { withFileTypes: true })
+  for (const entry of entries) {
+    const res = pathResolve(dir, entry.name)
+    if (entry.isDirectory()) {
+      yield * walkFiles(res)
+    }
+    else {
+      yield res
+    }
+  }
+}
 
 const name = "server-block"
 
@@ -14,9 +27,7 @@ const serverOutput = "server/.generated" as const
 export default defineNuxtModule({
   meta: {
     name,
-    compatibility: {
-      nuxt: ">=3.0.0"
-    }
+    compatibility: { nuxt: ">=3.0.0" }
   },
   async setup(userOptions, nuxt) {
     const { resolve } = createResolver(import.meta.url)
@@ -26,7 +37,8 @@ export default defineNuxtModule({
 
     // 0. Create directories and .gitignore
     const serverGeneratedDirectoryPath = resolve(nuxt.options.srcDir, "server/.generated")
-    if (!existsSync(serverGeneratedDirectoryPath)) await fsp.mkdir(serverGeneratedDirectoryPath)
+    if (existsSync(serverGeneratedDirectoryPath)) await fsp.rm(serverGeneratedDirectoryPath, { recursive: true })
+    await fsp.mkdir(serverGeneratedDirectoryPath, { recursive: true })
     await fsp.writeFile(`${serverGeneratedDirectoryPath}/.gitignore`, "*")
 
     // 1. Add Volar plugin
@@ -48,38 +60,43 @@ export default defineNuxtModule({
       )
     })
 
-    // 3.Watch directories, split handlers and add them to Nitro/Nuxt
     const allHandlers = new Map<string, NitroEventHandler>()
+
+    const addHandlers = async (path: string, event?: string) => {
+      if (!existsSync(path)) return // Return early if the path doesn't exist.
+      if (path.includes(serverOutput)) {
+        if (!path.endsWith(".ts")) return // skip non-ts files
+        if (path.includes("server/.generated/.loader")) return // skip files in .loader
+        if (SupportedMethods.map(m => `.${m.toLowerCase()}.ts`).some(s => path.includes(s))) return // skip .[method].ts
+        logger.info(`[update] '@${event}'`, shortened(path))
+        const route = getRoute(path) // This will throw if there's no generated handlers.
+        const file = await loadFile(path)
+        if (file) {
+          logger.info(`[update]: Adding new handler(s) @${route}`)
+          const handlers = await writeHandlers(file, path, serverGeneratedDirectoryPath)
+          for (const handler of handlers) {
+            logger.success(`[update] Wrote ${handler.method} handler @${handler.route} : ${shortened(handler.handler)}`)
+            allHandlers.set(handler.handler, handler)
+            if (useNitro().options.handlers.find(h => h.handler === handler.handler)) continue
+            useNitro().options.handlers.push({ ...handler, lazy: true })
+          }
+        }
+        // await useNuxt().hooks.callHookParallel("app:data:refresh") @todo find a way to refresh data here
+        logger.info("[update]: Nitro Handlers \n", useNitro().options.handlers.map(h => h.route))
+      }
+    }
+
+    // 3.Add handlers on build.
+    nuxt.hook("build:before", async () => {
+      for await (const loaderPath of walkFiles(serverGeneratedDirectoryPath)) {
+        await addHandlers(loaderPath, "build:before")
+      }
+    })
+
+    // 4.Watch directories, split handlers and add them to Nitro/Nuxt
     nuxt.hook("builder:watch", async (event, path) => {
       try {
-        if (!existsSync(path)) return // Return early if the path doesn't exist.
-        if (path.includes(serverOutput)) {
-          if (!path.endsWith(".ts")) return // skip non-ts files
-          if (path.includes("server/.generated/.loader")) return // skip files in .loader
-          if (SupportedMethods.map(m => `.${m.toLowerCase()}.ts`).some(s => path.includes(s))) return // skip .[method].ts
-          logger.info(`[update] '@${event}'`, shortened(path))
-          const route = getRoute(path) // This will throw if there's no generated handlers.
-          const file = await loadFile(path)
-          if (file) {
-            logger.info(`[update]: Adding new handler(s) @${route}`)
-            const handlers = await writeHandlers(file, path, serverGeneratedDirectoryPath)
-            for (const handler of handlers) {
-              logger.success(`[update] Wrote ${handler.method} handler @${handler.route} : ${shortened(handler.handler)}`)
-              allHandlers.set(handler.handler, handler)
-              if (!nuxt.options.serverHandlers.some(h => h.handler === handler.handler)) {
-                addServerHandler({ ...handler, lazy: true })
-                logger.success(`[update] Nuxt handler updated : ${handler.route}`)
-              }
-            }
-            for (const [path, handler] of allHandlers.entries()) {
-              if (useNitro().scannedHandlers.find(h => h.handler === path)) continue
-              useNitro().scannedHandlers.push({ ...handler, lazy: true })
-            }
-          }
-          // await useNuxt().hooks.callHookParallel("app:data:refresh") @todo find a way to refresh data here
-          logger.info("[update]: Nitro Handlers \n", useNitro().scannedHandlers.map(h => h.route))
-          logger.info("[update]: Nuxt Handlers \n", nuxt.options.serverHandlers.map(h => h.route))
-        }
+        await addHandlers(path, event)
       }
       catch (error) {
         logger.error(`error while handling '${event}'`, error)
