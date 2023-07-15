14b21edcc7bd62928da74668fae1e976ec4dfe02
Hebilicious <xsh4k3@gmail.com>
2023-07-15 16:18:13 +0700

    chore: add prisma playground (#7)

---
+++ playgrounds/prisma/pages/index.vue
@@ -0,0 +1,14 @@
+<server lang="ts">
+import { prisma } from '~/lib/prisma'
+
+export const GET = defineEventHandler(() => prisma.todo.findMany())
+</server>
+
+<script setup lang="ts">
+const { data, error } = useFetch("/api/index")
+</script>
+
+<template>
+  <div> asd {{ data }} </div>
+  <div> error {{ error }} </div>
+</template>
---
+++ playgrounds/prisma/prisma/seed.ts
@@ -0,0 +1,26 @@
+import type { Prisma } from "@prisma/client"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+const todoData: Prisma.TodoCreateInput[] = [{ title: "Milk", content: "Buy milk", completed: false }]
+
+async function main() {
+  // console.log("Start seeding ...")
+  for (const t of todoData) {
+    await prisma.todo.create({
+      data: t
+    })
+    // console.log(`Created todo with id: ${todo.id}`)
+  }
+  // console.log("Seeding finished.")
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
